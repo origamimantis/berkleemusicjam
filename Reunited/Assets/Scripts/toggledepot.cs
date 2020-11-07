0e70a50b52d50daf4dd6da34248311833550b447
eric <ezhang323@gmail.com>
2020-11-06 20:37:29 -0800

    disable-wall button

---
+++ Reunited/Assets/Scripts/toggledepot.cs
@@ -0,0 +1,42 @@
+﻿// Eric Zhang
+// 11/6/2020
+// this script makes collisions with an object with this behavior
+// (a button) disable collisions for <target>.
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toggledepot : MonoBehaviour
+{
+    // object to disble collisions for (a wall)
+    public GameObject[] targets;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+	foreach (GameObject target in targets)
+	{
+	    target.GetComponent<BoxCollider2D>().isTrigger = true;
+	    target.GetComponent<Renderer>().enabled = false;
+	}
+    }
+    private void OnTriggerExit2D(Collider2D col)
+    {
+	foreach (GameObject target in targets)
+	{
+	    target.GetComponent<BoxCollider2D>().isTrigger = false;
+	    target.GetComponent<Renderer>().enabled = true;
+	}
+    }
+}

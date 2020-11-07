dc35b00391736d4a234f93b64df2c3fafe092a88
Cornchip <cornchipgonecodin@gmail.com>
2020-11-07 14:55:25 -0500

    Resizing camera

---
+++ Reunited/Assets/Scripts/CameraController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject future;
+    public GameObject player;
+    public float lerp = 0.1f;
+    private Camera cam;
+
+    private void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector2 intermediate = new Vector2(future.transform.position.x + player.transform.position.x, future.transform.position.y + player.transform.position.y);
+        intermediate /= 2;
+
+        // -8.095001 -> 3.978115
+
+        // 12 = 5/12x
+        float dx = future.transform.position.x - player.transform.position.x;
+        float dy = future.transform.position.y - player.transform.position.y;
+
+        cam.orthographicSize += lerp * (Mathf.Max(5, 5 / 6.0f * Mathf.Sqrt(dx * dx + dy * dy)) - cam.orthographicSize);
+
+        Vector2 delta = intermediate - new Vector2(transform.position.x, transform.position.y);
+
+        delta *= lerp;
+        transform.position = new Vector3(transform.position.x + delta.x, transform.position.y + delta.y, transform.position.z);
+    }
+}

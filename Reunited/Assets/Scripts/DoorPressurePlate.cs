073c5cedcb3132691ca63be361581a4778759a4d
Cornchip <cornchipgonecodin@gmail.com>
2020-11-07 14:29:34 -0500

    Pressure Plate created

---
+++ Reunited/Assets/Scripts/DoorPressurePlate.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorPressurePlate : PressurePlate
+{
+    public GameObject[] openDoors;
+    public GameObject[] closeDoors;
+    
+    public override void onStep()
+    {
+        foreach (var item in openDoors)
+        {
+            item.SetActive(false);
+        }
+
+        foreach (var item in closeDoors)
+        {
+            item.SetActive(true);
+        }
+    }
+
+    public override void onLeave()
+    {
+        foreach (var item in openDoors)
+        {
+            item.SetActive(true);
+        }
+
+        foreach (var item in closeDoors)
+        {
+            item.SetActive(false);
+        }
+    }
+}

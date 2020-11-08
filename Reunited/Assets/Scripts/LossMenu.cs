459720557d1baa381be86850c1cfd33ada75e936
Brian Kiss <bjk4726@rit.edu>
2020-11-08 13:57:49 -0500

    Added retry screen.
    Put scripts that were outside the folder back inside as well.

---
+++ Reunited/Assets/Scripts/LossMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LossMenu : MonoBehaviour
+{
+    //static bool for use in other classes.
+    public GameObject lossMenuUI;
+
+    /// <summary>
+    /// Turns loss menu on, stops game.
+    /// </summary>
+    public void LossScreen()
+    {
+        lossMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    /// <summary>
+    /// Resets game
+    /// </summary>
+    public void ResetScene()
+    {
+        lossMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    /// <summary>
+    /// Loads menu from pause screen, resets timeScale.
+    /// </summary>
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
+    }
+}

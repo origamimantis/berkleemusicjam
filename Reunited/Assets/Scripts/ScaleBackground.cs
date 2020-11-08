c5e7d4b58f09bc3a5dbf1d54f959727fe0f31837
Brian Kiss <bjk4726@rit.edu>
2020-11-07 19:12:40 -0500

    Background img scales w/ screen size
    added buffer space to acount for animation.

---
+++ Reunited/Assets/Scripts/ScaleBackground.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//Brian Kiss
+//11/7/2020
+//Finds size of screen and scales background image to be slightly bigger than it (for animation).
+
+public class ScaleBackground : MonoBehaviour
+{
+    private float width;
+    private float height;
+
+    private float buffer = 100f;
+
+    public Image background;
+
+    /// <summary>
+    /// Gets width/height of screen, changes background image to fit.
+    /// </summary>
+    void Start()
+    {
+        width = Screen.width;
+        height = Screen.height;
+
+        background.rectTransform.sizeDelta = new Vector2(width + buffer, height + buffer);
+    }
+}

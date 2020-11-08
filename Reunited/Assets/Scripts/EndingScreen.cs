43192dd398d703b1f05abd903f689bff79f54e43
Cornchip <cornchipgonecodin@gmail.com>
2020-11-08 18:53:43 -0500

    Pushing

---
+++ Reunited/Assets/Scripts/EndingScreen.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndingScreen : MonoBehaviour
+{
+    public GameObject future;
+    public GameObject player;
+
+    public Camera cam;
+    public Light light;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

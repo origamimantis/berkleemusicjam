8dee8ee2c0ce6a30a8082d0edbe4dce64b9bfcb2
Cornchip <cornchipgonecodin@gmail.com>
2020-11-08 19:16:40 -0500

    End scene

--- Reunited/Assets/Scripts/EndingScreen.cs
+++ Reunited/Assets/Scripts/EndingScreen.cs
@@ -8,7 +8,9 @@ public class EndingScreen : MonoBehaviour
     public GameObject player;
 
     public Camera cam;
-    public Light light;
+    public new Light light;
+
+    float rampup = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -17,8 +19,35 @@ public class EndingScreen : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        rampup += Time.deltaTime;
+
+        if (rampup > 1)
+            rampup = 1;
+
+        player.transform.position = new Vector3(player.transform.position.x + rampup * Time.deltaTime * 0.3f * -player.transform.position.x, 0, -1);
+        future.transform.position = new Vector3(future.transform.position.x + rampup * Time.deltaTime * 0.3f * -future.transform.position.x, 0, 0);
+
+        float dx = Mathf.Abs(future.transform.position.x - player.transform.position.x);
+
+        if (dx < 3)
+        {
+            float sin = Mathf.Sin((3 - dx) / 3.0f * Mathf.PI);
+            cam.backgroundColor = new Color(255 * sin, 255 * sin, 255 * sin);
+
+            if (dx < 0.1 && sin < 0.05f)
+            {
+                light.intensity -= Time.deltaTime;
+            }
+            else
+                light.intensity = 1 + 5 * sin;
+        }
+
+
+        if(light.intensity <= 0)
+        {
+            // display text "fin"
+        }
     }
 }
